52717f09d779c9d7f97cd8bbb7c7ba4f5353fdf8
rami-platero <ramiplatero11@gmail.com>
2025-08-21 13:52:10 -0300

    chore: remove jiti config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,5 @@
 import type { NextConfig } from "next";
 import createNextIntlPlugin from "next-intl/plugin";
-import { fileURLToPath } from "node:url";
-import createJiti from "jiti";
-const jiti = createJiti(fileURLToPath(import.meta.url));
-
-jiti("./src/env/server.ts")
-jiti("./src/env/client.ts");
 
 const withNextIntl = createNextIntlPlugin()
 
