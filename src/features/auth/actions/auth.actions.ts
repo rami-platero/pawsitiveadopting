98c55c3aa3536c381f478ca4a0c766d9d52da163
rami-platero <ramiplatero11@gmail.com>
2025-11-27 22:13:14 -0300

    refactor: reorganize folder structure

--- src/features/auth/actions/auth.actions.ts
+++ src/features/auth/actions/auth.actions.ts
@@ -1,21 +1,21 @@
 "use server";
-import { auth } from "@/lib/auth"
- 
+import { auth } from "@/shared/lib/auth";
+
 export const signIn = async () => {
-    await auth.api.signInEmail({
-        body: {
-            email: "ramiplatero11@gmail.com",
-            password: "password",
-        }
-    })
-}
+  await auth.api.signInEmail({
+    body: {
+      email: "ramiplatero11@gmail.com",
+      password: "password",
+    },
+  });
+};
 
 export const signUp = async () => {
-    await auth.api.signUpEmail({
-        body: {
-            email: "ramiplatero11@gmail.com",
-            password: "password",
-            name: "Ramiro Platero",
-        }
-    })
-}
+  await auth.api.signUpEmail({
+    body: {
+      email: "ramiplatero11@gmail.com",
+      password: "password",
+      name: "Ramiro Platero",
+    },
+  });
+};
