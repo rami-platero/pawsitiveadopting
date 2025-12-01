164a6d7fce02079baf5fff4d85c93bd35ccee57b
rami-platero <ramiplatero11@gmail.com>
2025-12-01 16:20:59 -0300

    feat(intl): add dynamic translation for google button

--- src/features/auth/components/SignInWithGoogle.tsx
+++ src/features/auth/components/SignInWithGoogle.tsx
@@ -1,9 +1,18 @@
 "use client";
 
+import { Button } from "@/shared/components/ui/button";
 import { signInWithGoogle } from "@/shared/lib/auth-client";
+import { useTranslations } from "next-intl";
+import { FcGoogle } from "react-icons/fc";
 
 const SignInWithGoogle = () => {
-  return <button onClick={signInWithGoogle}>Sign In With Google</button>;
+
+  const t = useTranslations("AuthPage.LoginPage");
+
+  return <Button className="rounded-3xl w-full" onClick={signInWithGoogle}>
+    <FcGoogle />
+    {t("signInWithGoogle")}
+  </Button>;
 };
 
 export default SignInWithGoogle;
