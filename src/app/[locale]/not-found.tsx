ddf679e9eda04d7d2820dfe7240741ededa7e50e
rami-platero <ramiplatero11@gmail.com>
2025-12-01 16:16:24 -0300

    feat: add not found page component

---
+++ src/app/[locale]/not-found.tsx
@@ -0,0 +1,27 @@
+import Container from "@/shared/components/Container";
+import { Button, buttonVariants } from "@/shared/components/ui/button";
+import { cn } from "@/shared/lib/utils";
+import { Home } from "lucide-react";
+import { useTranslations } from "next-intl";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function NotFound() {
+  const t = useTranslations("NotFoundPage");
+
+  return (
+    <Container className="full-screen-center">
+      <Image alt="" src={"/assets/img/cat.png"} width={500} height={500} />
+      <div className="flex flex-col gap-4 items-center text-center">
+        <h1 className="md:text-5xl text-4xl font-medium max-w-xl">
+          {t("title")}
+        </h1>
+        <p className="text-gray-600 text-lg">{t("description")}</p>
+        <Link className={cn(buttonVariants({ size: "lg" }), "mt-4")} href={"/"}>
+          <Home className="h-4 w-4" />
+          {t("button")}
+        </Link>
+      </div>
+    </Container>
+  );
+}
