adec9c6c3783b64b646f1068d884bcc26151ebde
rami-platero <ramiplatero11@gmail.com>
2025-12-09 11:59:15 -0300

    refactor(db): conditionally swap db driver based on environment

--- apps/web/src/db/db.ts
+++ apps/web/src/db/db.ts
@@ -1,8 +1,22 @@
 import { env } from "@/env/server";
-import { drizzle } from 'drizzle-orm/neon-http';
-import { neon } from '@neondatabase/serverless';
+import { drizzle } from "drizzle-orm/neon-http";
+import { drizzle as drizzlePg } from "drizzle-orm/postgres-js";
+import { neon } from "@neondatabase/serverless";
 import * as schema from "./schema";
+import postgres from "postgres";
+import type { PostgresJsDatabase } from "drizzle-orm/postgres-js";
+import { NeonHttpDatabase } from "drizzle-orm/neon-http";
 
-const client = neon(env.DATABASE_URL);
+let db: PostgresJsDatabase<typeof schema> | NeonHttpDatabase<typeof schema>;
 
-export const db = drizzle({ client, schema });
+if (process.env.NODE_ENV == "development") {
+  const client = postgres(env.DATABASE_URL);
+
+  db = drizzlePg({ client, schema });
+} else {
+  const client = neon(env.DATABASE_URL);
+
+  db = drizzle({ client, schema });
+}
+
+export { db };
