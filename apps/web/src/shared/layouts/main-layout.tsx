f1d07cb454d1c3cdf97a1f647a9bb9f6b728bcc2
rami-platero <ramiplatero11@gmail.com>
2025-12-12 16:13:59 -0300

    chore(layout): import Footer component

--- apps/web/src/shared/layouts/main-layout.tsx
+++ apps/web/src/shared/layouts/main-layout.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import Navbar from "../components/navbar/navbar";
+import Footer from "@/shared/components/footer/Footer";
 
 type Props = {
   children: React.ReactNode;
 };
 
 const MainLayout = ({ children }: Props) => {
+
   return (
     <>
       <Navbar/>
       <main>{children}</main>
-      <footer></footer>
+      <Footer />
     </>
   );
 };
