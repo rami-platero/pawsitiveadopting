5e426f6d5892c7a180b6d848f5dc094499f8d711
rami-platero <ramiplatero11@gmail.com>
2025-12-12 18:03:49 -0300

    feat(seo): add generateMetadata function for notFound page

--- apps/web/src/app/[locale]/[...rest]/page.tsx
+++ apps/web/src/app/[locale]/[...rest]/page.tsx
@@ -1,4 +1,14 @@
 import { notFound } from "next/navigation";
+import { getTranslations } from "next-intl/server";
+
+export async function generateMetadata() {
+  const t = await getTranslations('Metadata.NotFound');
+
+  return {
+    title: t('title'),
+    description: t('description')
+  };
+}
 
 export default function CatchAllPage() {
   notFound();
