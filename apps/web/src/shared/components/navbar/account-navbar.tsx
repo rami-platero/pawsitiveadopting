ce9b0d04f2b650ca1e831d56f76179064bb6d970
rami-platero <ramiplatero11@gmail.com>
2025-12-12 10:57:11 -0300

    refactor: extract conditional rendering logic to account navbar component

--- apps/web/src/shared/components/navbar/account-navbar.tsx
+++ apps/web/src/shared/components/navbar/account-navbar.tsx
@@ -1,29 +1,37 @@
 import Image from "next/image"
 import Link from "next/link"
-import { signOut } from "@/features/auth/actions/auth.actions"
-import { LayoutDashboard, LogOutIcon, Settings } from "lucide-react"
+import { LayoutDashboard, Settings } from "lucide-react"
 import { cn } from "@pawsitiveadopting/ui/lib/utils";
-import { Button, buttonVariants } from "@pawsitiveadopting/ui/components/button"
+import { buttonVariants } from "@pawsitiveadopting/ui/components/button"
 import { DropdownMenu, DropdownMenuContent, DropdownMenuSeparator, DropdownMenuTrigger } from "@pawsitiveadopting/ui/components/dropdown-menu"
+import { getSession } from "@/features/auth/actions/server";
+import { getTranslations } from "next-intl/server";
+import SignOutButton from "@/features/auth/components/SignOutButton";
 
-type User = {
-  name?: string
-  email?: string
-  image?: string | null | undefined;
-}
+export default async function AccountNavbar() {
+  const session = await getSession()
+  const t = await getTranslations("Navbar")
 
-export default function AccountNavbar({
-  user,
-}: {
-  user: User
-}) {
+  if (!session) {
+    return <div className="flex items-center gap-2">
+      <Link
+        href={"/login"}
+        className={cn(buttonVariants({ variant: "ghost" }))}
+      >
+        {t("auth.login")}
+      </Link>
+      <Link href={"/sign-up"} className={cn(buttonVariants())}>
+        {t("auth.getStarted")}
+      </Link>
+    </div>
+  }
 
   return (
     <DropdownMenu modal={false}>
       <DropdownMenuTrigger>
         <Image
-          src={user.image ?? "/assets/img/default-avatar.png"}
-          alt={user.name ?? "avatar"}
+          src={session.user.image ?? "/assets/img/default-avatar.png"}
+          alt={session.user.name ?? "avatar"}
           width={40}
           height={40}
           className="object-cover cursor-pointer rounded-full"
@@ -33,15 +41,15 @@ export default function AccountNavbar({
       <DropdownMenuContent className="w-72 mr-2">
         <div className="px-2 py-1 flex gap-2 items-center">
           <Image
-            src={user.image ?? "/assets/img/default-avatar.png"}
-            alt={user.name ?? "avatar"}
+            src={session.user.image ?? "/assets/img/default-avatar.png"}
+            alt={session.user.name ?? "avatar"}
             width={32}
             height={32}
             className="object-cover rounded-full"
           />
           <div>
-            <h3 className="text-sm font-medium">{user.name ?? user.email}</h3>
-            <h4 className="text-xs text-muted-foreground">{user.email}</h4>
+            <h3 className="text-sm font-medium">{session.user.name ?? session.user.email}</h3>
+            <h4 className="text-xs text-muted-foreground">{session.user.email}</h4>
           </div>
 
         </div>
@@ -50,7 +58,7 @@ export default function AccountNavbar({
         <div className="flex flex-col gap-1">
           <Link href="/dashboard" className={cn(buttonVariants({ variant: "item" }))}><LayoutDashboard />Dashboard</Link>
           <Link href="/settings" className={cn(buttonVariants({ variant: "item" }))}><Settings />Settings</Link>
-          <Button onClick={signOut} size={"sm"} className="w-full font-regular"><LogOutIcon className="text-white" />Sign Out</Button>
+          <SignOutButton/>
         </div>
 
       </DropdownMenuContent>
