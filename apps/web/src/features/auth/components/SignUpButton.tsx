833163ca836f917ec64996ba78062ce4c8e7f0e7
rami-platero <ramiplatero11@gmail.com>
2025-12-13 08:55:36 -0300

    chore: remove unused auth buttons

--- apps/web/src/features/auth/components/SignUpButton.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { signUp } from "../actions/auth.actions";
-
-const SignUpButton = () => {
-  return <button onClick={signUp}>Sign up</button>
-};
-
-export default SignUpButton;
