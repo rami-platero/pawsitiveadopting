3c77e28019a3b9c874e2a55eb2a05b174efe4afa
rami-platero <ramiplatero11@gmail.com>
2025-12-20 18:14:38 -0300

    feat(auth): incorporate tabs from the AuthFlow component in sign-up and sign-in pages

--- apps/web/src/app/[locale]/(auth)/login/page.tsx
+++ apps/web/src/app/[locale]/(auth)/login/page.tsx
@@ -1,7 +1,10 @@
 import EmailLoginForm from "@/features/auth/components/login/EmailLoginForm";
+import AuthFlow from "@/features/auth/components/AuthFlow";
+import VerificationSent from "@/features/auth/components/VerificationSent";
 import SignInWithGoogle from "@/features/auth/components/SignInWithGoogle";
 import Container from "@/shared/components/Container";
 import Logo from "@/shared/components/Logo";
+import { TabsContent } from "@pawsitiveadopting/ui/components/tabs";
 import { getTranslations } from "next-intl/server";
 import Link from "next/link";
 
@@ -22,33 +25,42 @@ export async function generateMetadata() {
 
 export default async function LoginPage({ params }: PageProps) {
   const { locale } = await params;
-  const t = await getTranslations({locale, namespace: "AuthPage.LoginPage"});
+  const t = await getTranslations({ locale, namespace: "AuthPage.LoginPage" });
 
   return (
-    <Container className="full-screen-center gap-6 max-w-sm items-center">
-      <Logo size="lg" className="mx-auto" />
-      <div className="mb-2 mt-8 text-center">
-        <h1 className="font-medium text-xl">{t("title")}</h1>
-        <div className="text-sm text-secondary/80 mx-auto">
-          <span>{t("noAccount")}</span>
-          <Link
-            href="/signup"
-            className="text-secondary hover:underline font-medium"
-          >
-            {t("signUp")}
-          </Link>
-        </div>
-      </div>
-      <EmailLoginForm />
+    <Container className="full-screen-center gap-6 items-center justify-center my-4">
+      <AuthFlow>
 
-      {/* Divider */}
-      <div className="flex items-center w-full">
-        <div className="flex-1 h-px bg-gray-300"></div>
-        <span className="px-4 text-xs text-gray-500">{t("or")}</span>
-        <div className="flex-1 h-px bg-gray-300"></div>
-      </div>
+        <TabsContent value="entry" className="max-w-sm w-full mx-auto">
+          <Logo size="lg" className="mx-auto justify-center" />
+          <div className="mb-2 mt-8 text-center">
+            <h1 className="font-medium text-xl">{t("title")}</h1>
+            <div className="text-sm text-secondary/80 mx-auto">
+              <span>{t("noAccount")}</span>
+              <Link
+                href="/signup"
+                className="text-secondary hover:underline font-medium"
+              >
+                {t("signUp")}
+              </Link>
+            </div>
+          </div>
+          <EmailLoginForm />
 
-      <SignInWithGoogle />
+          <div className="flex items-center w-full my-4">
+            <div className="flex-1 h-px bg-gray-300"></div>
+            <span className="px-4 text-xs text-gray-500">{t("or")}</span>
+            <div className="flex-1 h-px bg-gray-300"></div>
+          </div>
+
+          <SignInWithGoogle />
+        </TabsContent>
+
+        <TabsContent value="email-verification" className="max-w-xl mx-auto">
+          <VerificationSent />
+        </TabsContent>
+
+      </AuthFlow>
     </Container>
   );
 }
--- apps/web/src/app/[locale]/(auth)/sign-up/page.tsx
+++ apps/web/src/app/[locale]/(auth)/sign-up/page.tsx
@@ -1,54 +1,60 @@
-import EmailRegisterForm from "@/features/auth/components/register/EmailRegisterForm";
-import SignInWithGoogle from "@/features/auth/components/SignInWithGoogle";
-import Container from "@/shared/components/Container";
-import Logo from "@/shared/components/Logo";
-import { getTranslations } from "next-intl/server";
-import Link from "next/link";
-
-type PageProps = {
-  params: {
-    locale: string
-  }
-}
+import { getTranslations } from "next-intl/server"
+import { TabsContent } from "@pawsitiveadopting/ui/components/tabs"
+import EmailRegisterForm from "@/features/auth/components/register/EmailRegisterForm"
+import Logo from "@/shared/components/Logo"
+import Link from "next/link"
+import SignInWithGoogle from "@/features/auth/components/SignInWithGoogle"
+import Container from "@/shared/components/Container"
+import VerificationSent from "@/features/auth/components/VerificationSent"
+import AuthFlow from "@/features/auth/components/AuthFlow"
 
 export async function generateMetadata() {
   const t = await getTranslations('Metadata.SignUp');
-
   return {
     title: t('title'),
     description: t('description')
   };
 }
 
-export default async function SignUpPage({ params }: PageProps) {
-  const { locale } = await params;
-  const t = await getTranslations({ locale, namespace: "AuthPage.SignUpPage" });
+export default async function SignUpPage({ params }: { params: { locale: string } }) {
+  const { locale } = await params
+  const t = await getTranslations({ locale, namespace: "AuthPage.SignUpPage" })
 
   return (
-    <Container className="full-screen-center gap-6 max-w-sm items-center">
-      <Logo size="lg" className="mx-auto" />
-      <div className="mb-2 mt-8 text-center">
-        <h1 className="font-medium text-xl">{t("title")}</h1>
-        <div className="text-sm text-secondary/80 mx-auto">
-          <span>{t("alreadyAccount")}</span>
-          <Link
-            href="/login"
-            className="text-secondary hover:underline font-medium"
-          >
-            {t("logIn")}
-          </Link>
-        </div>
-      </div>
-      <EmailRegisterForm />
+    <Container className="full-screen-center gap-6 items-center justify-center my-4">
+      <AuthFlow>
 
-      {/* Divider */}
-      <div className="flex items-center w-full">
-        <div className="flex-1 h-px bg-gray-300"></div>
-        <span className="px-4 text-xs text-gray-500">{t("or")}</span>
-        <div className="flex-1 h-px bg-gray-300"></div>
-      </div>
+        <TabsContent value="entry" className="max-w-sm mx-auto">
+          <Logo size="lg" className="mx-auto justify-center" />
+          <div className="mb-2 mt-8 text-center">
+            <h1 className="font-medium text-xl">{t("title")}</h1>
 
-      <SignInWithGoogle />
+            <div className="text-sm text-secondary/80 mx-auto mb-4">
+              <span>{t("alreadyAccount")}</span>
+              <Link
+                href="/login"
+                className="text-secondary hover:underline font-medium"
+              >
+                {t("logIn")}
+              </Link>
+            </div>
+          </div>
+          <EmailRegisterForm />
+
+          <div className="flex items-center w-full my-4">
+            <div className="flex-1 h-px bg-gray-300"></div>
+            <span className="px-4 text-xs text-gray-500">{t("or")}</span>
+            <div className="flex-1 h-px bg-gray-300"></div>
+          </div>
+
+          <SignInWithGoogle />
+        </TabsContent>
+
+        <TabsContent value="email-verification" className="max-w-xl mx-auto">
+          <VerificationSent />
+        </TabsContent>
+
+      </AuthFlow>
     </Container>
-  );
+  )
 }
