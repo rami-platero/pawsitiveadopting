808b5441d077c33a2acf8ba1ddc39f963b112730
rami-platero <ramiplatero11@gmail.com>
2025-12-26 13:30:24 -0300

    feat(utils): add function to create verification email URL and custom locale extraction

--- apps/web/src/shared/utils/auth.utils.ts
+++ apps/web/src/shared/utils/auth.utils.ts
@@ -1,12 +1,35 @@
-export function extractAuthAction(url?: string): 'send-verification-email' | 'sign-up' | null {
+import { env } from "@/env/server";
+
+export function extractAuthAction(
+  url?: string
+): "send-verification-email" | "sign-up" | null {
   if (!url) return null;
 
   const pathname = new URL(url).pathname;
-  const parts = pathname.split('/');
+  const parts = pathname.split("/");
 
   const action = parts[3];
 
-  return action === 'send-verification-email' || action === 'sign-up'
+  return action === "send-verification-email" || action === "sign-up"
     ? action
     : null;
+}
+
+export function createURL({
+  token,
+  email,
+}: {
+  token: string;
+  email: string;
+}): string {
+  return `${env.BASE_URL}/confirm?token=${token}&email=${encodeURIComponent(email)}`;
+}
+
+export function extractLocale(headers: Headers | undefined) {
+  const cookieHeader = headers?.get("cookie") || "";
+  const localeCookie = cookieHeader
+    .split(";")
+    .find((c) => c.trim().startsWith("NEXT_LOCALE="));
+
+  return localeCookie?.split("=")[1] || "en";
 }
