5a9e7e5e7fbf5fa2be05e31c8ded57012c8ae01a
rami-platero <ramiplatero11@gmail.com>
2025-12-26 13:41:05 -0300

    test: pass params to HomePage component

--- apps/web/src/__tests__/page.test.tsx
+++ apps/web/src/__tests__/page.test.tsx
@@ -1,14 +1,14 @@
 import { render, } from "@testing-library/react";
 import messages from "../../messages/en.json"
 import { NextIntlClientProvider } from "next-intl";
-import {describe, it} from 'vitest';
-import HomePage from "@/app/[locale]/page";
+import { describe, it } from 'vitest';
+import HomePage from "@/app/[locale]/(main)/page";
 
 describe("Home", () => {
   it("renders a page", () => {
     render(
       <NextIntlClientProvider locale="en" messages={messages}>
-        <HomePage />
+        <HomePage params={{ locale: "en" }} />
       </NextIntlClientProvider>
     );
   });
