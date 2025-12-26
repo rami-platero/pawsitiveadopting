511e500ac8dd03dac45fad4098394d33e8fa8647
rami-platero <ramiplatero11@gmail.com>
2025-12-26 13:38:10 -0300

    refactor: use client and server providers, and replace MainLayout with raw custom layout

--- apps/web/src/app/[locale]/layout.tsx
+++ apps/web/src/app/[locale]/layout.tsx
@@ -1,18 +1,19 @@
 import { hasLocale } from "next-intl";
 import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
-import Providers from "@/context/providers";
-import MainLayout from "@/shared/layouts/main-layout";
 import { Jost } from "next/font/google";
 import "@pawsitiveadopting/tailwind-config/globals.css";
 import { setRequestLocale } from "next-intl/server";
+import Navbar from "@/shared/components/navbar/navbar";
+import ClientProviders from "@/context/clientProviders";
+import ServerProviders from "@/context/serverProviders";
 
 const jost = Jost({
   subsets: ["latin"],
 });
 
 export function generateStaticParams() {
-  return routing.locales.map((locale) => ({locale}));
+  return routing.locales.map((locale) => ({ locale }));
 }
 
 export default async function LocaleLayout({
@@ -32,9 +33,12 @@ export default async function LocaleLayout({
   return (
     <html lang={locale} className={jost.className}>
       <body className="bg-background selection:bg-primary/50 selection:text-secondary">
-        <Providers>
-          <MainLayout>{children}</MainLayout>
-        </Providers>
+        <ServerProviders>
+          <ClientProviders>
+            <Navbar />
+            {children}
+          </ClientProviders>
+        </ServerProviders>
       </body>
     </html>
   );
