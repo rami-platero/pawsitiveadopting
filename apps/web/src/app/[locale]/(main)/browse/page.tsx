fe743d087aec8b6da88db0deb5afb03583a651d1
rami-platero <ramiplatero11@gmail.com>
2026-01-13 16:15:11 -0300

    feat: add components and data fetching to browse page

---
+++ apps/web/src/app/[locale]/(main)/browse/page.tsx
@@ -0,0 +1,50 @@
+import { getTranslations } from 'next-intl/server';
+import Filters from '@/features/pets/components/filters/filters';
+import { Suspense } from 'react';
+import PostsGridSkeleton from '@/features/pets/components/posts-grid-skeleton';
+import { searchParamsSchema } from '@/features/pets/schema/searchParams.schema';
+import { parseParams } from '@/features/pets/utils/parser.helper';
+import { redirect } from 'next/navigation';
+import { getAvailableFilters } from '@/features/pets/data-access/getFilters';
+import FilteredPostsResults from '@/features/pets/components/filtered-posts-results';
+
+export async function generateMetadata() {
+  const t = await getTranslations('Metadata.Browse');
+
+  return {
+    title: t('title'),
+    description: t('description')
+  };
+}
+
+type Props = {
+  searchParams: Promise<{ search?: string, type?: string }>;
+}
+
+export default async function BrowsePage({ searchParams }: Props) {
+  const resolvedParams = await searchParams;
+  const parsedParams = parseParams({
+    schema: searchParamsSchema,
+    searchParams: resolvedParams, onInvalidParams: (safeURL: string) => {
+      redirect(safeURL)
+    }
+  });
+  const availableFilters = await getAvailableFilters({ type: parsedParams.type, breed: parsedParams.breed, age: parsedParams.age });
+
+  return (
+    <div className="py-8">
+      <div className="mb-8">
+        <h1 className="text-3xl font-bold mb-2">Browse Pets</h1>
+        <p className="text-muted-foreground">Find your perfect companion</p>
+      </div>
+
+      <div className='lg:flex gap-4'>
+        <Filters availableFilters={availableFilters} />
+        <Suspense key={JSON.stringify(parsedParams)} fallback={<PostsGridSkeleton />}>
+          <FilteredPostsResults filters={parsedParams} />
+        </Suspense>
+      </div>
+
+    </div>
+  )
+}
